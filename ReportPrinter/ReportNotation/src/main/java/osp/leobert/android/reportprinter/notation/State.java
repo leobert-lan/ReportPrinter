7ff14cd4ca51c02f8cde98641e867b686b06d0b2
leobert-lan <774057695@qq.com>
2018-07-08 15:08:24 +0800

    release 1.0.0

---
+++ ReportPrinter/ReportNotation/src/main/java/osp/leobert/android/reportprinter/notation/State.java
@@ -0,0 +1,12 @@
+package osp.leobert.android.reportprinter.notation;
+
+/**
+ * <p><b>Package:</b> osp.leobert.android.reportprinter.notation </p>
+ * <p><b>Project:</b> ReportPrinter </p>
+ * <p><b>Classname:</b> State </p>
+ * <p><b>Description:</b> TODO </p>
+ * Created by leobert on 2018/7/7.
+ */
+public enum State {
+    TODO, DOING, SOLVED
+}
