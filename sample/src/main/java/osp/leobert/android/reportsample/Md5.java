d359a2bfc691efb80d1b5f6001bfeb5a13ffcf33
leobert-lan <774057695@qq.com>
2020-06-03 19:57:58 +0800

    migrate to androidX

---
+++ sample/src/main/java/osp/leobert/android/reportsample/Md5.java
@@ -0,0 +1,110 @@
+package osp.leobert.android.reportsample;
+
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import osp.leobert.android.utils.reporter.Util;
+
+/**
+ * <p><b>Package:</b> osp.leobert.android.reportsample </p>
+ * <p><b>Project:</b> ReportPrinter </p>
+ * <p><b>Classname:</b> Md5 </p>
+ * <p><b>Description:</b> TODO </p>
+ * Created by leobert on 2020/6/3.
+ */
+public class Md5 {
+    private static final String TAG = "Md5";
+
+    public static boolean checkMD5(String md5, File updateFile) {
+        if (TextUtils.isEmpty(md5) || updateFile == null) {
+//            L.e(TAG, "MD5 string empty or updateFile null");
+            return false;
+        }
+
+        String calculatedDigest = calculateMD5(updateFile);
+        if (calculatedDigest == null) {
+//            Log.e(TAG, "calculatedDigest null");
+            return false;
+        }
+
+//        Log.v(TAG, "Calculated digest: " + calculatedDigest);
+//        Log.v(TAG, "Provided digest: " + md5);
+
+        return calculatedDigest.equalsIgnoreCase(md5);
+    }
+
+
+    @Util(group = Util.UtilGroup.MD5, usage = "文件内容计算md5")
+    public static String calculateMD5(File updateFile) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+//            Log.e(TAG, "Exception while getting digest", e);
+            return null;
+        }
+
+        InputStream is;
+        try {
+            is = new FileInputStream(updateFile);
+        } catch (FileNotFoundException e) {
+//            Log.e(TAG, "Exception while getting FileInputStream", e);
+            return null;
+        }
+
+        byte[] buffer = new byte[8192];
+        int read;
+        try {
+            while ((read = is.read(buffer)) > 0) {
+                digest.update(buffer, 0, read);
+            }
+            byte[] md5sum = digest.digest();
+            BigInteger bigInt = new BigInteger(1, md5sum);
+            String output = bigInt.toString(16);
+            // Fill to 32 chars
+            output = String.format("%32s", output).replace(' ', '0');
+            return output;
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to process file for MD5", e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+//                Log.e(TAG, "Exception on closing MD5 input stream", e);
+            }
+        }
+    }
+
+    @Util(group = Util.UtilGroup.MD5, usage = "字符串计算md5")
+    public static String calculateMD5(String psw) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(psw.getBytes("UTF-8"));
+            byte[] encryption = md5.digest();
+
+            StringBuilder strBuf = new StringBuilder();
+            for (int i = 0; i < encryption.length; i++) {
+                if (Integer.toHexString(0xff & encryption[i]).length() == 1) {
+                    strBuf.append("0").append(Integer.toHexString(0xff & encryption[i]));
+                } else {
+                    strBuf.append(Integer.toHexString(0xff & encryption[i]));
+                }
+            }
+
+            return strBuf.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return "";
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+}
