d3be9087a89da0a243d93d8997f8fb9aeb59f9b8
leobert <leobert.l@hotmail.com>
2022-09-22 11:24:14 +0800

    feat: nothing

--- samples/kotlin_sample/src/main/java/osp/leobert/android/kotlin_sample/diagram/FutureTaskDemo.kt
+++ samples/kotlin_sample/src/main/java/osp/leobert/android/kotlin_sample/diagram/FutureTaskDemo.kt
@@ -4,7 +4,11 @@ import osp.leobert.android.reporter.diagram.notation.ClassDiagram
 import osp.leobert.android.reporter.diagram.notation.GenerateClassDiagram
 import osp.leobert.android.reporter.diagram.notation.IgnoreExclude
 import osp.leobert.android.reporter.diagram.notation.Visible
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.FutureTask
+import java.util.concurrent.RecursiveAction
+import java.util.concurrent.RecursiveTask
+import java.util.concurrent.RunnableScheduledFuture
 
 /**
  * Classname: FutureTaskDemo </p>
@@ -13,8 +17,10 @@ import java.util.concurrent.FutureTask
  */
 
 @ClassDiagram(
-    qualifier = "FutureTask", fieldVisible = [Visible.Package, Visible.Public],
+    qualifier = "FutureTask", fieldVisible = [/*Visible.Package, */Visible.Public],
+    methodVisible=[Visible.Public],
     ignoreExclude = IgnoreExclude(
+        exactFullNames = ["java.lang.Runnable"],
         fullNamePatterns = ["java\\.util\\.concurrent\\.\\w"]
     )
 )
@@ -23,6 +29,14 @@ annotation class FutureTaskDiagram
 
 @GenerateClassDiagram
 @FutureTaskDiagram
-class FutureTaskDemo {
+class FutureTaskDemo(
+    private val futureTask: FutureTask<String>,
+    private val rsf:RunnableScheduledFuture<String>,
+    private val rt:RecursiveTask<String>,
+    val a:CompletableFuture<String>,
+    val b:RecursiveTask<String>,
+    val c:RecursiveAction
+) {
     var task: FutureTask<String>? = null
+
 }
